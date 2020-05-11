1c928c341d0b5532f8a544abdc32f436e9e90089
Bence Dányi <Bence.Danyi@ibm.com>
2020-05-11 20:18:19 +0200

    feat: add programmatic api

--- Collector.js
+++ Collector.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const yaml = require('yaml');
 const glob = require('fast-glob');
 
-const collector = (paths) => {
+const collector = async (paths) => {
   const docs = [];
-  const files = glob.sync(paths);
+  const files = await glob(paths);
 
   for (const file of files) {
-    for (const doc of yaml.parseAllDocuments(fs.readFileSync(file, 'utf-8'))) {
+    const content = await fs.promises.readFile(file, 'utf8');
+    for (const doc of yaml.parseAllDocuments(content)) {
       docs.push({
         content: doc.toJSON(),
         doc,
