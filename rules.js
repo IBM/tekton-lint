55c90ec20dfb76f9a057e71f7966b44b685dd7ae
Bence Dányi <Bence.Danyi@ibm.com>
2020-08-15 00:30:12 +0200

    refactor: remove duplicated `no-task-duplicated-params` rule

--- rules.js
+++ rules.js
@@ -93,19 +93,7 @@ module.exports.lint = function lint(docs, reporter) {
   runRule('no-task-undefined-params');
   runRule('no-task-unused-params');
   runRule('no-condition-unused-params');
-
-  for (const task of Object.values(tekton.tasks)) {
-    const params = getTaskParams(task.spec);
-    if (!params) continue;
-    const paramNames = new Set();
-    for (const param of params) {
-      if (!paramNames.has(param.name)) {
-        paramNames.add(param.name);
-      } else {
-        error(`Task '${task.metadata.name}' has a duplicated param: '${param.name}'.`, param, 'name');
-      }
-    }
-  }
+  runRule('no-task-duplicated-params');
 
   for (const task of Object.values(tekton.tasks)) {
     let volumes = [];
