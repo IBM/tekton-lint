18941a74a9f4a9118dd6ca4b69254a13a104c331
Bence Dányi <Bence.Danyi@ibm.com>
2020-08-14 23:53:13 +0200

    refactor: remove duplicated `no-params-api-mix` rule

--- rules.js
+++ rules.js
@@ -172,30 +172,7 @@ module.exports.lint = function lint(docs, reporter) {
     }
   }
 
-  for (const task of Object.values(tekton.tasks)) {
-    switch (task.apiVersion) {
-      case 'tekton.dev/v1alpha1':
-        if (task.spec.params) error(`Task '${task.metadata.name}' is defined with apiVersion tekton.dev/v1alpha1, but defines spec.params. Use spec.inputs.params instead.`, task.spec.params);
-        break;
-      case 'tekton.dev/v1beta1':
-        if (task.spec.inputs && task.spec.inputs.params) error(`Task '${task.metadata.name}' is defined with apiVersion tekton.dev/v1beta1, but defined spec.inputs.params. Use spec.params instead.`, task.spec.inputs.params);
-        break;
-    }
-  }
-
-  for (const pipeline of Object.values(tekton.pipelines)) {
-    for (const task of pipeline.spec.tasks) {
-      if (!task.taskSpec) continue;
-      switch (pipeline.apiVersion) {
-        case 'tekton.dev/v1alpha1':
-          if (task.taskSpec.params) error(`Pipeline '${pipeline.metadata.name}' is defined with apiVersion tekton.dev/v1alpha1, but defines an inlined task (${task.name}) with spec.params. Use spec.inputs.params instead.`, task.taskSpec.params);
-          break;
-        case 'tekton.dev/v1beta1':
-          if (task.taskSpec.inputs && task.taskSpec.inputs.params) error(`Pipeline '${pipeline.metadata.name}' is defined with apiVersion tekton.dev/v1beta1, but defines an inlined task (${task.name}) with spec.inputs.params. Use spec.params instead.`, task.taskSpec.inputs.params);
-          break;
-      }
-    }
-  }
+  runRule('no-params-api-mix');
 
   for (const pipeline of Object.values(tekton.pipelines)) {
     errorCyclesInPipeline(pipeline, [runAfterReferences, paramsReferences, resourceInputReferences], error);
