b430a779c63e432f15896fcaec111861d6b5f777
Bence Dányi <Bence.Danyi@ibm.com>
2020-08-15 01:56:22 +0200

    refactor: remove duplicated template workspace rule

--- rules.js
+++ rules.js
@@ -69,25 +69,7 @@ module.exports.lint = function lint(docs, reporter) {
   runRule('no-template-pipeline-missing-parameters');
   runRule('no-pipeline-task-missing-workspace');
   runRule('no-pipeline-task-missing-reference');
-
-  for (const pipeline of Object.values(tekton.pipelines)) {
-    if (!pipeline.spec.workspaces) continue;
-    const required = pipeline.spec.workspaces.map(ws => ws.name);
-
-    for (const template of Object.values(tekton.triggerTemplates)) {
-      const pipelineRuns = template.spec.resourcetemplates.filter(item => item.spec && item.spec.pipelineRef && item.spec.pipelineRef.name === pipeline.metadata.name);
-
-      for (const pipelineRun of pipelineRuns) {
-        const provided = pipelineRun.spec.workspaces || [];
-
-        for (const workspace of required) {
-          if (!provided.find(ws => ws.name === workspace)) {
-            error(`TriggerTemplate '${template.metadata.name}' references Pipeline '${pipeline.metadata.name}', but provides no workspace for '${workspace}' (it's a required workspace in '${pipeline.metadata.name}')`, pipelineRun.spec.workspaces || pipelineRun.spec);
-          }
-        }
-      }
-    }
-  }
+  runRule('no-template-pipeline-missing-workspace');
 
   for (const triggerBinding of Object.values(tekton.triggerBindings)) {
     if (!triggerBinding.spec || !triggerBinding.spec.params) continue;
