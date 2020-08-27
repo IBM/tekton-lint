066c9d31e46c82f70bb4642808d260f6fc7ca67a
Szabolcs Szabolcsi-Tóth <szabolcsit@hu.ibm.com>
2020-08-27 22:02:14 +0200

    fix: inline pipeline spec should not break the linter

--- rules/no-template-missing-pipeline.js
+++ rules/no-template-missing-pipeline.js
@@ -2,6 +2,9 @@ module.exports = (docs, tekton, report) => {
   for (const template of Object.values(tekton.triggerTemplates)) {
     for (const resourceTemplate of template.spec.resourcetemplates) {
       if (resourceTemplate.kind !== 'PipelineRun') continue;
+
+      if (resourceTemplate.spec.pipelineSpec) continue;
+
       if (!tekton.pipelines[resourceTemplate.spec.pipelineRef.name]) {
         report(`TriggerTemplate '${template.metadata.name}' references pipeline '${resourceTemplate.spec.pipelineRef.name}', but the referenced pipeline cannot be found.`, resourceTemplate.spec.pipelineRef, 'name');
       }
