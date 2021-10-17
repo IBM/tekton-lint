07c93fddec28089cb6453f9c1b09c40b7065a7f8
Tamas Muncsan <tamas.muncsan@ibm.com>
2021-10-17 13:27:41 +0200

    fix: report `conditions` in embedded pipelines

--- src/rules/prefer-when-expression.ts
+++ src/rules/prefer-when-expression.ts
@@ -7,4 +7,18 @@ export default (docs, tekton, report) => {
       }
     }
   }
+
+  for (const triggerTemplate of Object.values<any>(tekton.triggerTemplates)) {
+    for (const crd of triggerTemplate.spec.resourcetemplates) {
+      if (crd.kind !== 'PipelineRun') continue;
+      const pipelineSpec = crd.spec.pipelineSpec;
+      if (!pipelineSpec || !pipelineSpec.tasks) continue;
+      for (const task of pipelineSpec.tasks) {
+        if (task.conditions) {
+          const guardedBy = task.conditions.map(condition => condition.conditionRef);
+          report(`Task '${task.name}' in PipelineRun '${crd.metadata.name}' is guarded by condition(s) ('${guardedBy.join(', ')}'). Conditions are deprecated, use WhenExpressions instead.`, task, 'conditions');
+        }
+      }
+    }
+  }
 };
